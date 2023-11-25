b1695013136cc76d33f76f6ae9f6816c2343e709
cleivison-pinheiro <cleivisoncontabilidade@gmail.com>
2023-11-25 17:26:01 -0400

    Desafio Heroi Criado

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 
-let nome = "cleivison"
+let nomeDoHeroi = "Homen de Ferro";
+let xpDoHeroi = 5000;
+let nivel;
 
-console.log(nome)
+if (xpDoHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpDoHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpDoHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpDoHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpDoHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpDoHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpDoHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivel}`);
